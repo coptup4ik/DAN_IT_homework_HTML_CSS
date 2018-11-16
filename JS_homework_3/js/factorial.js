a88f4fa3d7f648638125bd5e40ab1802fb545319
pavel_lebedev <designer.lebedev@gmail.com>
2018-11-16 21:38:30 +0200

    JS homework3

--- JS_homework_3/js/factorial.js
+++ JS_homework_3/js/factorial.js
@@ -5,7 +5,6 @@
 
 const
   FACTORIAL = document.querySelector('#factorial'),
-  CHECK_BTN = document.querySelector('#factorialBtn'),
   MAIN_ELEMENT = document.querySelector('.form'),
   NUMBERS = document.querySelector('.numbers');
 
@@ -14,31 +13,35 @@ function factorial(number) {
   if (number === 0) {
     return 1;
   } else {
-    return number * factorial(number - 1)
+    return number * factorial(number - 1);
   }
 }
 
 function checkInputs(value) {
   if (!Number.isInteger(+value) || isNaN(+value)) {
     MAIN_ELEMENT.classList.add('error');
-    return false
+    return false;
   } else {
     MAIN_ELEMENT.classList.remove('error');
     NUMBERS.textContent = "";
-    return true
+    return true;
   }
 }
 
 function generateFactorial(value) {
 
   if (checkInputs(value)) {
-    NUMBERS.textContent = factorial(+value)
+    NUMBERS.textContent = factorial(+value);
   }
 
 }
 
-CHECK_BTN.addEventListener('click', () => {
-  generateFactorial(FACTORIAL.value);
+FACTORIAL.addEventListener('keypress', (event) => {
+  if (event.charCode === 13) {
+    event.preventDefault();
+    generateFactorial(FACTORIAL.value);
+  }
+
 });
 
 
